7e0c9f15aa089f36eeb284d21b839279a3458851
onexie <dcloudnetwork@gmail.com>
2022-12-29 19:43:16 +0800

    添加部分协议转换功能

--- net/util/util.go
+++ net/util/util.go
@@ -3,8 +3,10 @@ package util
 import (
 	"sync"
 
+	"github.com/dcnetio/gothreads-lib/core/thread"
 	apipb "github.com/dcnetio/gothreads-lib/net/api/pb"
 	netpb "github.com/dcnetio/gothreads-lib/net/pb"
+	ma "github.com/multiformats/go-multiaddr"
 )
 
 func RecFromServiceRec(r *netpb.Log_Record) *apipb.Record {
@@ -23,6 +25,46 @@ func RecToServiceRec(r *apipb.Record) *netpb.Log_Record {
 		HeaderNode: r.HeaderNode,
 		BodyNode:   r.BodyNode,
 	}
+}
+
+// LogToProto returns a proto log from a thread log.
+func LogToProto(l thread.LogInfo) *netpb.Log {
+	return &netpb.Log{
+		ID:      &netpb.ProtoPeerID{ID: l.ID},
+		PubKey:  &netpb.ProtoPubKey{PubKey: l.PubKey},
+		Addrs:   AddrsToProto(l.Addrs),
+		Head:    &netpb.ProtoCid{Cid: l.Head.ID},
+		Counter: l.Head.Counter,
+	}
+}
+
+// LogFromProto returns a thread log from a proto log.
+func LogFromProto(l *netpb.Log) thread.LogInfo {
+	return thread.LogInfo{
+		ID:     l.ID.ID,
+		PubKey: l.PubKey.PubKey,
+		Addrs:  AddrsFromProto(l.Addrs),
+		Head: thread.Head{
+			ID:      l.Head.Cid,
+			Counter: l.Counter,
+		},
+	}
+}
+
+func AddrsToProto(mas []ma.Multiaddr) []netpb.ProtoAddr {
+	pas := make([]netpb.ProtoAddr, len(mas))
+	for i, a := range mas {
+		pas[i] = netpb.ProtoAddr{Multiaddr: a}
+	}
+	return pas
+}
+
+func AddrsFromProto(pa []netpb.ProtoAddr) []ma.Multiaddr {
+	mas := make([]ma.Multiaddr, len(pa))
+	for i, a := range pa {
+		mas[i] = a.Multiaddr
+	}
+	return mas
 }
 
 func NewSemaphore(capacity int) *Semaphore {
